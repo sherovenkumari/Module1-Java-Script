2d65ae398140b13ec30cdb931546c48af3b81341
sherovenkumari <sherovenkumari311@gmail.com>
2025-05-26 23:20:52 +0530

    Add files via upload

---
+++ Que2.js
@@ -0,0 +1,8 @@
+
+const eventName = "Music Fest";
+const eventDate = "2025-06-15";
+let availableSeats = 50;
+
+console.log(`${eventName} is on ${eventDate}. Seats available: ${availableSeats}`);
+
+availableSeats--; // One user registered
