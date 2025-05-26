3608b507ef296afe1d62d086e16d5f37936553a4
sherovenkumari <sherovenkumari311@gmail.com>
2025-05-26 23:29:07 +0530

    Add files via upload

---
+++ Que14.js
@@ -0,0 +1,8 @@
+
+// jQuery
+$('#registerBtn').click(() => alert('Registered!'));
+$('.eventCard').fadeIn();
+$('.eventCard').fadeOut();
+
+// Framework Benefit
+// Using frameworks like React or Vue improves modularity, performance, and maintainability through component-based architecture.
